0054846e0e02c62b1aa41d6f2c82731ca9c9a355
John Boone <j.raditya.b@gmail.com>
2022-11-21 15:27:32 +0800

    5th commit

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,7 @@
 class Movie < ApplicationRecord
   has_many :bookmarks
-  validates :title, uniqueness: true, length: { minimum: 1 }
-  validates :overview, uniqueness: true, length: { minimum: 1 }
+  has_many :lists, through: :bookmarks
+
+  validates :title, uniqueness: true, presence: true
+  validates :overview, uniqueness: true, presence: true
 end
